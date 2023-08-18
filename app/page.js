b936912a6c6c259810b988588e8e7baefa9fe424
Prathamesh Sirdesai <sirdesai.work@gmail.com>
2023-08-18 21:27:45 +0530

    challenge completed

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Checklist from "../components/Checklist";
 export default function Home() {
   return (
-    <main className=" w-full bg-teal-700 min-h-screen  items-center justify-between p-24">
+    <main className=" w-full bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen  items-center justify-between p-24">
       <Checklist />
     </main>
   );
